eed09f83ad7bc16fb8c238814404e28d562e3585
Timothy Weaver <tlweave2@asu.edu>
2026-02-18 00:27:09 +0000

    housecall

---
+++ housecallpro-config.js
@@ -0,0 +1,12 @@
+/**
+ * HouseCall Pro Configuration
+ * Do not store secrets here. HouseCall Pro booking works from the public bookingUrl.
+ * apiKey/merchantId left empty on purpose so this file can be safely tracked.
+ */
+
+window.HOUSECALLPRO_CONFIG = {
+  bookingUrl: 'https://book.housecallpro.com/book/Ragsdales-Heat--Air/024a1eee9a1744658998fb8b5e9b2af5?v2=true&lead_source=website',
+  apiKey: '',
+  merchantId: '',
+  companyId: '024a1eee9a1744658998fb8b5e9b2af5'
+};
